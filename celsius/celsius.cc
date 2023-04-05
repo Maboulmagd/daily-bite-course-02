511a6c79fc41beacb540ef7ba93906bf194f510b
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2023-04-05 07:39:47 +0000

    Adding celsius solutions

--- celsius/celsius.cc
+++ celsius/celsius.cc
@@ -1,5 +1,10 @@
 #include "celsius.h"
 
-double to_celsius(double fahrenheit) {}
+double to_celsius(double fahrenheit) {
+  return (fahrenheit - 32.0) * (5.0 / 9.0);
+}
 
-double to_fahrenheit(double celsius) {}
+double to_fahrenheit(double celsius) {
+  return ((9.0 * celsius) / 5.0) +
+         32.0; // Avoid any implicit conversions by continuing to use doubles
+}
